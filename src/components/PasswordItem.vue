b0c348cf07ef132f090776f5d58df8cf46589b24
Ender Imen <eimen@boynergrup.com>
2021-01-07 15:44:33 +0300

    Show The detail information of the account in the active password tab is displayed.

---
+++ src/components/PasswordItem.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="m-password__item js-tab">
+        <app-text size="small" weight="bold" class="-mb10">{{ password.title }}</app-text>
+        <app-text size="small" weight="thin">{{ password.name }}</app-text>
+    </div>
+</template>
+
+<script>
+import appText from "@/components/Text.vue";
+export default {
+    components: {
+        appText
+    },
+    props: {
+        password: Object
+    }
+}
+</script>
+
+<style lang="scss">
+.m-password__item {
+    height: 80px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    padding-left: 20px;
+    cursor: pointer;
+    transition: ease-in 300ms;
+    border-bottom: 1px solid var(--line-bg-color);
+
+    &.-active {
+        color: var(--light-text-color);
+        background-color: var(--active-tab-bg-color);
+        border: 0
+    }
+
+    &:not(.-active) {
+      &:hover {
+        transition: ease-out 300ms;
+        background-color: var(--soft-content-bg-color);
+      }
+    }
+}
+</style>
--- src/views/MyPassword.vue
+++ src/views/MyPassword.vue
@@ -1,35 +1,21 @@
 <template>
   <section class="m-password">
       <section class="m-password__list">
-        <div class="m-password__item">
+        <div class="m-password__searchArea">
           <input type="text" class="m-password__search -mr20" placeholder="Hesap ya da şifre ara">
         </div>
-        <div class="m-password__item -active">
-          <app-text size="small" weight="bold" class="-mb10">Google Gmail</app-text>
-          <app-text size="small" weight="thin">imen.ender@gmail.com</app-text>
-        </div>
-        <div class="m-password__item">
-          <app-text size="small" weight="bold" class="-mb10">Google Gmail</app-text>
-          <app-text size="small" weight="thin">imen.ender@gmail.com</app-text>
-        </div>
-        <div class="m-password__item">
-          <app-text size="small" weight="bold" class="-mb10">Google Gmail</app-text>
-          <app-text size="small" weight="thin">imen.ender@gmail.com</app-text>
-        </div>
-        <div class="m-password__item">
-          <app-text size="small" weight="bold" class="-mb10">Google Gmail</app-text>
-          <app-text size="small" weight="thin">imen.ender@gmail.com</app-text>
-        </div>
+
+        <app-password-item @click.native="currentAccount" v-for="password in passwordList" :key="password.id" :password="password" :data-id="password.id"></app-password-item>
       </section>
 
       <section class="m-password__detail">
         <div class="m-password__summary">
           <app-text size="small" weight="bold" class="-mb10" color="soft">Kullanıcı Adı</app-text>
-          <app-text size="small" weight="thin" class="-mb20">imen.ender@gmail.com</app-text>
-          <app-text size="small" weight="bold" class="-mb10" color="soft">Şifre</app-text>
+          <app-text size="small" weight="thin" class="-mb20">{{ passwordDetail.name }}</app-text>
+          <app-text size="small" weight="bold" class="-mb10" color="soft">Parola</app-text>
 
           <div class="group">
-            <input type="password" value="dassaasdasda" class="m-password__input" disabled>
+            <input type="password" :value="passwordDetail.password" class="m-password__input" disabled>
             <IconCopy class="icon -soft"/>
           </div>
         </div>
@@ -37,14 +23,14 @@
         <div class="m-password__info">
           <app-text size="small" weight="bold" class="-mb10" color="soft">Website</app-text>
           <a href="https://www.gmail.com" class="-mb20">
-            <app-text size="small" weight="thin" color="soft" >https://www.gmail.com</app-text>
+            <app-text size="small" weight="thin" color="soft" >{{ passwordDetail.website }}</app-text>
           </a>
 
           <app-text size="small" weight="bold" class="-mb10" color="soft">Last Modified</app-text>
-          <app-text size="small" weight="thin" color="soft" class="-mb20">Today at 12:09 PM</app-text>
+          <app-text size="small" weight="thin" color="soft" class="-mb20">{{ passwordDetail.lastModified }}</app-text>
 
           <app-text size="small" weight="bold" class="-mb10" color="soft">created</app-text>
-          <app-text size="small" weight="thin" color="soft" class="-mb20">Today at 12:09 PM</app-text>
+          <app-text size="small" weight="thin" color="soft" class="-mb20">{{ passwordDetail.created }}</app-text>
         </div>
       </section>
   </section>
@@ -53,11 +39,75 @@
 <script>
 import appText from "@/components/Text.vue";
 import IconCopy from "@/icons/copy.svg";
+import appPasswordItem from '@/components/PasswordItem.vue';
 
 export default {
   components: {
     appText,
-    IconCopy
+    IconCopy,
+    appPasswordItem
+  },
+  data() {
+    return {
+      passwordList: [
+        {
+          id: 1,
+          title: "Google Gmail",
+          name: "imen.ender@gmail.com",
+          password: "sdasdasdasasdsdsdasdasds",
+          website: "https://www.gmail.com",
+          lastModified: "Today at 12:09 PM",
+          created: "Today at 03:25 AM"
+        },
+        {
+          id: 2,
+          title: "Google Gmail",
+          name: "imen.ender@gmail.com",
+          password: "sdasdasdasaasdasdasd",
+          website: "https://www.gmail.com",
+          lastModified: "Today at 12:09 PM",
+          created: "Today at 02:25 AM"
+        },
+        {
+          id: 3,
+          title: "Google Gmail",
+          name: "imen.ender@gmail.com",
+          password: "sdasda",
+          website: "https://www.gmail.com",
+          lastModified: "Today at 12:09 PM",
+          created: "Today at 07:25 AM"
+        }
+      ],
+      passwordDetail: {
+          id: 1,
+          title: "Test Account",
+          name: "test@gmail.com",
+          password: "sdasdasdasasdsdsdasdas",
+          website: "https://www.gee.com",
+          lastModified: "Today at 12:09 PM",
+          created: "Today at 03:25 AM"
+        }
+    }
+  },
+  methods: {
+    currentAccount(event) {
+      const tabList = document.querySelectorAll(".js-tab");
+
+      tabList.forEach((passwordItem) => {
+        if(passwordItem.classList.contains("-active")) {
+          passwordItem.classList.remove("-active");
+        }
+      });
+
+      event.target.classList.add("-active");
+      this.showDetailPassword(event.target.dataset.id);
+    },
+    showDetailPassword(passwordId) {
+      const currentPassword = this.passwordList.find((passwordItem) => {
+        return parseInt(passwordId) === passwordItem.id
+      });
+      this.passwordDetail = currentPassword;
+    }
   }
 }
 </script>
@@ -71,6 +121,15 @@ export default {
   height: calc(100% - var(--header-gap) - 10px);
 
   &__search {
+    &Area {
+      height: 80px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      padding-left: 20px;
+      border-bottom: 1px solid var(--line-bg-color);
+    }
+
     box-sizing: border-box;
     padding: 15px 20px;
     height: 50px;
@@ -91,29 +150,6 @@ export default {
   &__list {
     flex: 1;
     border-right: 1px solid var(--line-bg-color);
-  }
-
-  &__item {
-    height: 80px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    padding-left: 20px;
-    cursor: pointer;
-    transition: ease-in 300ms;
-    border-bottom: 1px solid var(--line-bg-color);
-
-    &.-active {
-      color: var(--light-text-color);
-      background-color: var(--active-tab-bg-color);
-    }
-
-    &:not(.-active) {
-      &:hover {
-        transition: ease-out 300ms;
-        background-color: var(--soft-content-bg-color);
-      }
-    }
   }
 
   &__detail {
