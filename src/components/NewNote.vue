3bf5e6940d8d22e12a4ee7cd6026b2d11946126d
Ender Imen <eimen@boynergrup.com>
2021-01-08 22:56:55 +0300

    Note components have been completed.

---
+++ src/components/NewNote.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="m-newNote">
+      <label for="title">
+        <app-text class="-mb10" color="soft">Başlık giriniz</app-text>
+        <input id="title" type="text" class="m-newNote__title" placeholder="Başlık giriniz">
+      </label>
+      <textarea class="m-newNote__type" placeholder="Notunuzu yazmaya başlayın..."></textarea>
+  </section>
+</template>
+
+<script>
+import appText from './Text.vue'
+export default {
+  components: { appText },
+}
+</script>
+
+<style lang="scss">
+.m-newNote {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+
+    & label {
+        display: flex;
+        flex-direction: column;
+    }
+
+    &__title {
+        height: 40px;
+        margin-bottom: 30px;
+        padding-left: 15px;
+        padding-right: 15px;
+    }
+
+    &__type {
+        width: 100%;
+        padding: 30px;
+        border: 1px solid var(--line-bg-color);
+        resize: none;
+        margin-bottom: 30px;
+        text-align: justify;
+        flex: 2;
+    }
+}
+</style>
--- src/views/MyNotes.vue
+++ src/views/MyNotes.vue
@@ -1,113 +1,21 @@
 <template>
   <section class="m-note">
     <div class="m-note__list">
-      <div class="m-note__item">
-        <header class="m-note__header">
-          <app-text size="small" weight="bold">Test Notu</app-text>
-          <IconDelete class="icon -black"/>
-        </header>
-        <article class="m-note__content">
-          Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta, expedita. Lorem ipsum, dolor sit. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, voluptates. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore adipisci nisi repellendus consectetur suscipit rem maxime culpa ex molestias. Esse!
-        </article>
-      </div>
-
-      <div class="m-note__item">
-        <header class="m-note__header">
-          <app-text size="small" weight="bold">Test Notu</app-text>
-          <IconDelete class="icon -black"/>
-        </header>
-        <article class="m-note__content">
-          Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta, expedita. Lorem ipsum, dolor sit.
-        </article>
-      </div>
-
-      <div class="m-note__item">
-        <header class="m-note__header">
-          <app-text size="small" weight="bold">Test Notu</app-text>
-          <IconDelete class="icon -black"/>
-        </header>
-        <article class="m-note__content">
-          Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta, expedita. Lorem ipsum, dolor sit.
-        </article>
-      </div>
-
-      <div class="m-note__item">
-        <header class="m-note__header">
-          <app-text size="small" weight="bold">Test Notu</app-text>
-          <IconDelete class="icon -black"/>
-        </header>
-        <article class="m-note__content">
-          Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta, expedita. Lorem ipsum, dolor sit
-        </article>
-      </div>
-
-      <div class="m-note__item">
-        <header class="m-note__header">
-          <app-text size="small" weight="bold">Test Notu</app-text>
-          <IconDelete class="icon -black"/>
-        </header>
-        <article class="m-note__content">
-          Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta, expedita. Lorem ipsum, dolor sit
-        </article>
-      </div>
-
-      <div class="m-note__item">
-        <header class="m-note__header">
-          <app-text size="small" weight="bold">Test Notu</app-text>
-          <IconDelete class="icon -black"/>
-        </header>
-        <article class="m-note__content">
-          Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta, expedita. Lorem ipsum, dolor sit
-        </article>
-      </div>
-      <div class="m-note__item">
-        <header class="m-note__header">
-          <app-text size="small" weight="bold">Test Notu</app-text>
-          <IconDelete class="icon -black"/>
-        </header>
-        <article class="m-note__content">
-          Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta, expedita. Lorem ipsum, dolor sit
-        </article>
-      </div>
-      <div class="m-note__item">
-        <header class="m-note__header">
-          <app-text size="small" weight="bold">Test Notu</app-text>
-          <IconDelete class="icon -black"/>
-        </header>
-        <article class="m-note__content">
-          Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta, expedita. Lorem ipsum, dolor sit
-        </article>
-      </div>
-      <div class="m-note__item">
-        <header class="m-note__header">
-          <app-text size="small" weight="bold">Test Notu</app-text>
-          <IconDelete class="icon -black"/>
-        </header>
-        <article class="m-note__content">
-          Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta, expedita. Lorem ipsum, dolor sit
-        </article>
-      </div>
-      <div class="m-note__item">
-        <header class="m-note__header">
-          <app-text size="small" weight="bold">Test Notu</app-text>
-          <IconDelete class="icon -black"/>
-        </header>
-        <article class="m-note__content">
-          Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta, expedita. Lorem ipsum, dolor sit
-        </article>
-      </div>
+      <app-note-item v-for="note in getNoteList()" :key="note.id" :note="note"></app-note-item>
     </div>
   </section>
 </template>
 
 <script>
-import appText from '@/components/Text.vue';
-import IconDelete from '@/icons/delete.svg';
+import { mapGetters } from "vuex";
+import appNoteItem from '../components/NoteItem.vue';
 
 export default {
     components: {
-        appText,
-        IconDelete
+        appNoteItem
+    },
+    methods: {
+      ...mapGetters(["getNoteList"])
     }
 }
 </script>
@@ -122,46 +30,7 @@ export default {
   &__list {
     display: flex;
     flex-wrap: wrap;
-    align-content: flex-start;
-    height: inherit;
     width: 100%;
-  }
-
-  &__item {
-    height: 200px;
-    margin-bottom: 60px;
-    width: 33.3333%;
-    border-radius: 5px;
-    padding: 20px;
-    cursor: pointer;
-  }
-
-  &__header {
-    height: 35px;
-    padding-left: 10px;
-    padding-right: 10px;
-    background-color: #FBB901;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    font-family: var(--font-family);
-  }
-
-  &__content {
-    padding: 13px;
-    height: inherit;
-    text-align: justify;
-    background-color: #f7dc59;
-
-    background-image: url("https://www.transparenttextures.com/patterns/lined-paper.png");
-
-    // url("https://www.transparenttextures.com/patterns/skulls.png");
-
-    // url("https://www.transparenttextures.com/patterns/xv.png");
-
-    // url("https://www.transparenttextures.com/patterns/white-leather.png");
-
-    // url("https://www.transparenttextures.com/patterns/flowers.png");
   }
 }
 </style>
