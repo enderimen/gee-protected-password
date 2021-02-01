175f4cdb154937f1f591a4d32937686b28c31cfc
Ender Imen <eimen@boynergrup.com>
2021-02-01 16:40:09 +0300

    Vue router added to project.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueResource from "vue-resource"
 import "./styles/app.scss"
+
+Vue.use(VueResource);
 
 const language = localStorage.getItem("language");
 document.documentElement.lang = language;
